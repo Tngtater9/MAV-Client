1e65a48768e76871518fe344f0bd385f3c162361
Jamie Tater <jtater1@gmail.com>
2020-07-17 20:42:56 -0400

    simplified screens

--- src/App.js
+++ src/App.js
@@ -49,6 +49,22 @@ class App extends React.Component {
         <div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
       </div>
       <div ref={el => this.mapContainer = el} className="mapContainer" />
+      <form class="AVAST_PAM_nonloginform">
+        <label forHTML="title">Title</label>
+        <input type="text" placeholder="Title of appointment"/><br/>
+        <label forHTML="date">Date</label>
+        <input type="text" id="date" name="date" placeholder="Appointment date" onfocus="(this.type='date')"/><br/>
+        <label forHTML="time">Time</label>
+        <input type="text" id="time" name="time" placeholder="Appointment Time" onfocus="(this.type='time')"/><br/>
+        <label forHTML="address">Address</label>
+        <input type="text" id="address" name="address" placeholder="Appointment address"/><br/>
+        <label forHTML="details">Details</label>
+        <textarea id="details" name="details" placeholder="Appointment details"></textarea><br/>
+        <div>
+            <button type="submit">[Checkmark Icon]</button>
+            <button type="reset">[X mark icon]</button><br/><br/>
+        </div>
+    </form>
     </div>
   )}
 }
