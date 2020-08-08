55e2d546c58343c34f0557dbb36fdf74b78a333d
Jamie Tater <jtater1@gmail.com>
2020-08-08 13:32:51 -0400

    added landing styling

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -6,8 +6,7 @@ function LandingPage () {
     return (
         <main>
             <p className="mission">MAV, which stands for Mobile Appointment Viewer creates a map with markers representing appointments for a given day. The user can see where their daily appointments are and add new appointments to the route. This idea was spawned as a way to cut down on travel back and forth across the city so the user can save time and money.</p>
-            <img className="map" src={map} alt="MAV Screenshot"></img>
-            <p className="demo">[Placeholder for video clip showing demo for MAV use]</p>
+            <img className="map" src={map} alt="MAV Screenshot" />
         </main>
     )
 }
